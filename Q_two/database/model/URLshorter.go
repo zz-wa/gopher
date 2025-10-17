170dc5855733029d29ce92b616d6a1e1ac17b83b
zz <1372234588@QQ.COM>
2025-10-17 17:30:03 +0800

    Delete Q_two/database directory

--- Q_two/database/model/URLshorter.go
+++
@@ -1,9 +0,0 @@
-package model
-
-import "gorm.io/gorm"
-
-type URLShorter struct {
-	gorm.Model
-	OriginalURL string `gorm:"not null" json:"original_url"`
-	ShortURL    string `gorm:"not null" json:"short_url"`
-}
